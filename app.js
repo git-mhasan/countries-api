f9949dd05a955febacbf46e63b342dd08837e351
git-mhasan <mahadi2020dev@gmail.com>
2022-02-24 08:55:55 +0600

    css card designed.

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("hello")
+function loadCountry() {
+    fetch("https://restcountries.com/v3.1/all")
+        .then(response => response.json())
+        .then(data => displayCountry(data));
+}
+
+loadCountry()
+
+const displayCountry = (countries) => {
+    const countriesContainer = document.getElementById("countries");
+    countries.forEach(country => {
+        // const thumbnailDiv = document.createElement('div');
+        // thumbnailDiv.classList.add('thumbnail');
+
+        // const img = document.createElement('img');
+        // img.src = 
+        console.log(country.name.common);
+
+    });
+}
