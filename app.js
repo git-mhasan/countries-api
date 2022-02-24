ea273421a2d7c2a25fc97cf661997ee52240bfd7
git-mhasan <mahadi2020dev@gmail.com>
2022-02-24 09:03:20 +0600

    display flags of all country using api with JS.

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ loadCountry()
 const displayCountry = (countries) => {
     const countriesContainer = document.getElementById("countries");
     countries.forEach(country => {
-        // const thumbnailDiv = document.createElement('div');
-        // thumbnailDiv.classList.add('thumbnail');
+        const thumbnailDiv = document.createElement('div');
+        thumbnailDiv.classList.add('thumbnail');
 
-        // const img = document.createElement('img');
-        // img.src = 
-        console.log(country.name.common);
-
+        const img = document.createElement('img');
+        img.src = country.flags.svg;
+        thumbnailDiv.appendChild(img);
+        const h6Tag = document.createElement('h6');
+        h6Tag.innerText = country.name.common;
+        thumbnailDiv.appendChild(h6Tag);
+        countriesContainer.appendChild(thumbnailDiv);
     });
 }
